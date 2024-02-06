ca411560587a16d1e82bd5e8fff321a6be9eeb19
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-02-06 18:10:29 -0500

    add skeleton html and  basic cal func in js

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtraction(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function division(num1, num2){
+    return num1 / num2;
+}
